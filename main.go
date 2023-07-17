6d19236ac0fdaaba623be5d944c114c66acb7976
ernesto <ernestosolanoballena@hotmail.com>
2023-07-17 16:33:26 -0500

    prueba de encriptacion

--- main.go
+++ main.go
@@ -4,149 +4,103 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
-	"fmt"
-	"io/ioutil"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
-var encryptionKey = []byte("clave_de_encriptacion")
+func encryptFile(filename string, key []byte) error {
+	// Abrir el archivo original
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-func caesarEncrypt(text string, shift int) string {
-	encrypted := strings.Builder{}
-	shift = shift % 26 // Para evitar valores de desplazamiento mayores a 26
+	// Crear un archivo temporal para almacenar los datos encriptados
+	tempFile, err := os.Create(filename + ".enc")
+	if err != nil {
+		return err
+	}
+	defer tempFile.Close()
 
-	for _, char := range text {
-		if char >= 'a' && char <= 'z' {
-			encrypted.WriteByte(byte((int(char-'a')+shift)%26 + 'a'))
-		} else if char >= 'A' && char <= 'Z' {
-			encrypted.WriteByte(byte((int(char-'A')+shift)%26 + 'A'))
-		} else {
-			encrypted.WriteByte(byte(char))
+	// Crear el bloque AES
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	// Generar un vector de inicialización único
+	iv := make([]byte, aes.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return err
+	}
+
+	// Escribir el vector de inicialización en el archivo temporal
+	if _, err := tempFile.Write(iv); err != nil {
+		return err
+	}
+
+	// Crear el modo de cifrado en CBC
+	stream := cipher.NewCFBEncrypter(block, iv)
+
+	// Leer el archivo original y encriptar los datos
+	buffer := make([]byte, 4096)
+	for {
+		n, err := file.Read(buffer)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if n == 0 {
+			break
+		}
+		encryptedData := make([]byte, n)
+		stream.XORKeyStream(encryptedData, buffer[:n])
+		if _, err := tempFile.Write(encryptedData); err != nil {
+			return err
 		}
 	}
 
-	return encrypted.String()
-}
-
-func encryptFile(filePath string, shift int, key []byte) error {
-	data, err := ioutil.ReadFile(filePath)
-	if err != nil {
+	// Eliminar el archivo original
+	if err := os.Remove(filename); err != nil {
 		return err
 	}
 
-	encryptedData := caesarEncrypt(string(data), shift)
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
+	// Renombrar el archivo temporal al nombre original
+	if err := os.Rename(filename+".enc", filename); err != nil {
 		return err
-	}
-
-	iv := make([]byte, aes.BlockSize)
-	if _, err := rand.Read(iv); err != nil {
-		return err
-	}
-
-	aesgcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return err
-	}
-
-	ciphertext := aesgcm.Seal(nil, iv, []byte(encryptedData), nil)
-
-	encryptedFileData := append(iv, ciphertext...)
-
-	newFilePath := strings.TrimSuffix(filePath, filepath.Ext(filePath)) + ".encrypted"
-	err = ioutil.WriteFile(newFilePath, encryptedFileData, 0644)
-	if err != nil {
-		return err
-	}
-
-	err = os.Remove(filePath)
-	if err != nil {
-		fmt.Println("No se pudo eliminar el archivo original:", err)
 	}
 
 	return nil
 }
 
-func decryptFile(filePath string, shift int, key []byte) error {
-	data, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return err
-	}
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return err
-	}
-
-	iv := data[:aes.BlockSize]
-	ciphertext := data[aes.BlockSize:]
-
-	aesgcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return err
-	}
-
-	plaintext, err := aesgcm.Open(nil, iv, ciphertext, nil)
-	if err != nil {
-		return err
-	}
-
-	decryptedData := caesarEncrypt(string(plaintext), -shift)
-
-	newFilePath := strings.TrimSuffix(filePath, ".encrypted") // Eliminar la extensión .encrypted
-	err = ioutil.WriteFile(newFilePath, []byte(decryptedData), 0644)
-	if err != nil {
-		return err
-	}
-
-	err = os.Remove(filePath)
-	if err != nil {
-		fmt.Println("No se pudo eliminar el archivo encriptado:", err)
-	}
-
-	return nil
+func encryptFolder(folderPath string, key []byte) error {
+	err := filepath.Walk(folderPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			err := encryptFile(path, key)
+			if err != nil {
+				log.Printf("Error encriptando archivo %s: %v\n", path, err)
+			} else {
+				log.Printf("Archivo %s encriptado exitosamente\n", path)
+			}
+		}
+		return nil
+	})
+	return err
 }
 
 func main() {
-	folderPath := ""
-	extension := ".txt"
-	shift := 3
+	folderPath := "/ruta/a/carpeta"                            // Ruta a la carpeta que deseas encriptar
+	key := []byte("clave_de_encriptacion_de_16_24_o_32_bytes") // Clave de encriptación AES (16, 24 o 32 bytes)
 
-	files, err := ioutil.ReadDir(folderPath)
+	err := encryptFolder(folderPath, key)
 	if err != nil {
-		fmt.Println("Error al leer la carpeta:", err)
-		return
+		log.Fatalf("Error encriptando carpeta: %v\n", err)
 	}
 
-	fmt.Print("Ingrese el código de acceso para desencriptar los archivos: ")
-	var accessCode string
-	fmt.Scanln(&accessCode)
-
-	if accessCode == "tu_clave_secreta" {
-		for _, file := range files {
-			if strings.HasSuffix(file.Name(), ".encrypted") {
-				filePath := folderPath + "/" + file.Name()
-				err := decryptFile(filePath, shift, encryptionKey)
-				if err != nil {
-					fmt.Println("Error al desencriptar el archivo", filePath, ":", err)
-				} else {
-					fmt.Println("Archivo desencriptado:", strings.TrimSuffix(filePath, ".encrypted"))
-				}
-			} else if strings.HasSuffix(file.Name(), extension) {
-				filePath := folderPath + "/" + file.Name()
-				err := encryptFile(filePath, shift, encryptionKey)
-				if err != nil {
-					fmt.Println("Error al encriptar el archivo", filePath, ":", err)
-				} else {
-					fmt.Println("Archivo encriptado:", filePath+".encrypted")
-				}
-			}
-		}
-	} else {
-		fmt.Println("Código de acceso incorrecto. No se puede desencriptar.")
-	}
+	log.Println("Carpeta encriptada exitosamente")
 }
